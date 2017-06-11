9802039f1139eded26590724890e381551765161
Matthew Myers <matt2east@gmail.com>
2017-06-10 19:32:58 -0500

    adds 1 styled message

--- mockup/script.js
+++ mockup/script.js
@@ -1,8 +1,22 @@
-function myFunction() {
-    var x = event.keyCode;
+function addChat() {
+    var keysend = event.keyCode;
     var inputvalue = document.getElementById("inputchat").value;
-    if (x == 13 && inputvalue != "") {
-        alert(inputvalue)
+//    var newtext = document.createTextNode(inputvalue);
+    var node = document.createElement("div");
+    var textnode = document.createTextNode(inputvalue);
+    
+    if (keysend == 13 && inputvalue != "") {
+//    console.log(inputvalue)
+    node.appendChild(textnode);
+    document.getElementById("newchat").appendChild(node);
+    var d = document.getElementById("newchat");
+        d.className += " arrow_box";    
+    document.getElementById("inputchat").value = "";  
+    
+
+      
+    
+        
     } 
 
 }
