e995e1bd19e11b386ce02e99aa4c8e8644db39ed
Matthew Myers <matt2east@gmail.com>
2017-06-20 20:17:47 -0500

    prints time

--- mockup/script.js
+++ mockup/script.js
@@ -1,4 +1,8 @@
-var arr = []
+var arr = [];
+var currentTime = new Date();
+var cls = document.getElementsByClassName('time');
+
+
 
 function addChat() {
     var keysend = event.keyCode;
@@ -8,31 +12,28 @@ function addChat() {
     if (keysend == 13 && inputvalue != "") {
         arr.push(inputvalue);
         var addArr = arr;
-
         addArr = addArr.map(function(e) {
-            return '<div class="arrow_box">' + e + '</div>'})
-//        alert("array items are " + addArr);
+            return '<div class="arrow_box">' + e + '<div class="time"></div></div>'})
         for (var i in addArr) {
             
         }
         
-// for each item in addArr create a styled div with the message
 
-//        var newmessage = document.createElement("div");
-//        var messagetext = document.createTextNode(addArr[i]);
-//        newmessage.appendChild(messagetext);
         var div = document.getElementById("newchat");
         div.innerHTML = div.innerHTML + (addArr[i])
-//        var styling = document.getElementById("newchat");
-//        styling.className += " arrow_box";    
-//    document.getElementById("inputchat").value = "";
-        document.getElementById("myform").reset();    
+      for (var i = 0; i < cls.length; i++) 
+    cls[i].innerHTML= (currentTime);
+        document.getElementById("myform").reset(); 
+        
+     
          
     } 
     
 
 }
 
-var div = document.getElementById('divID');
 
-div.innerHTML = div.innerHTML + 'Extra stuff';
+
+for (var i = 0; i < cls.length; i++) 
+    cls[i].innerHTML= (currentTime);
+
